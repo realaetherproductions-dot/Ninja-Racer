fa79de7503eb7e05a1931a93bffff47bd9fda4e3
Alejandro Villa <elteboalejo@gmail.com>
2026-04-23 16:12:47 -0400

    Add start screen, audio, and mobile flow fixes

--- sw.js
+++ sw.js
@@ -1,24 +1,36 @@
-const CACHE = "ninjacycle-v2";
+const CACHE = "ninjacycle-v3";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./sw.js"];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
       Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") {
+    return;
+  }
+
+  if (event.request.mode === "navigate") {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE).then((cache) => cache.put("./index.html", copy));
+          return response;
+        })
+        .catch(() => caches.match("./index.html"))
+    );
     return;
   }
 
